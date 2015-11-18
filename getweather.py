3a91c965981bdf68e40d1867c04d1abb35fda032
andytopham <andytspare@gmail.com>
2015-11-18 08:07:56 +0000

    Bug fix to getweather
    String concatenation error

--- getweather.py
+++ getweather.py
@@ -20,7 +20,7 @@ print 'Fetching weather info'
 MyUoled.writerow(1,'Weather for '+constants.locn)
 temp,wind,winddir,obs = MyWeather.wunder(constants.key,constants.locn)
 print 'Results',temp, wind, winddir, obs	
-MyUoled.writerow(2,'Temperature:'+temp' C')
+MyUoled.writerow(2,'Temperature:'+temp+' C')
 MyUoled.writerow(3,'Wind:'+wind+'mph. Dirn:'+winddir)
 MyUoled.writerow(4,'Observation:'+obs)
 MyUoled.display()
